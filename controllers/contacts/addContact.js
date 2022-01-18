64cbb9fbd6e9c7fedd7e427cfc6e38f866cfba5d
Volodymyr Klochkov <klochkov1954@gmail.com>
2022-01-19 00:46:51 +0200

    deleting console.log when addingContact

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,6 @@
 const contactsOperations = require("../../model/contacts");
 
 const addContact = async (req, res) => {
-  console.log(req.body);
   const result = await contactsOperations.addContact(req.body);
   res.status(201).json({
     status: "success",
