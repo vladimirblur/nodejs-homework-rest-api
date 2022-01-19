b047aa5ad20ae69e74bb84c51dfe7a67a9574393
Volodymyr Klochkov <klochkov1954@gmail.com>
2022-01-19 23:43:21 +0200

    adding .env in gitignore and del him from remote repo, and deleting unnecessary verification in addContact

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,12 +1,7 @@
 const { Contact } = require("../../models");
-const { BadRequest } = require("http-errors");
 
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
-
-  if (Object.keys(req.body).length === 0) {
-    throw new BadRequest(`missing fields`);
-  }
 
   res.status(201).json({
     status: "success",
